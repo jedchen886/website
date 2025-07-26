b7e6ccb334641c900173390805252eccc2f7a84c
jedchen886 <chen_lizhong@yeah.net>
2025-07-27 00:44:09 +0800

    modified the header and footer.

--- src/navigation.ts
+++ src/navigation.ts
@@ -50,13 +50,11 @@ export const headerData = {
     },
     {
       text: 'DORA开源社区', //DORA Open Source Community
-      href: '/',
-
+      href: 'http://www.dora-rs.ai',
     },
     {
       text: '服务与支持', //Services & Support
       href: '/',
-
     },
     {
       text: '关于我们', //About Us
@@ -88,11 +86,88 @@ export const footerData = {
     //   ],
     // },
     {
-      title: 'KIPPAL Robotics',
+      title: '产品',
       links: [
-        { text: 'Home', href: '/' },
-        { text: 'Blog', href: '/blog' },
-        { text: 'About', href: '/' },
+        {
+          text: 'ADORA2 Max',
+          href: '/#adora2-max',
+        },
+        {
+          text: 'ADORA2 Pro',
+          href: '/#adora2-pro',
+        },
+        {
+          text: 'ADORA2 Mini',
+          href: '/#adora2-mini',
+        },
+        {
+          text: 'ADORA1 Pro',
+          href: '/#adora1-pro',
+        },
+        {
+          text: 'ADORA1 Mini',
+          href: '/#adora1-mini',
+        },
+      ],
+    },
+    {
+      title: 'DORA开源社区',
+      links: [
+        {
+          text: 'DORA开源社区', //DORA Open Source Community
+          href: 'http://www.dora-rs.ai',
+      },
+      ],
+    },
+    {
+      title: '关于我们',
+      links: [
+        {
+          text: '公司简介',
+          href: '/',
+        },
+        {
+          text: '新闻中心',
+          href: '/',
+        },
+        {
+          text: '联系我们',
+          href: '/',
+        },
+      ],
+    },
+    {
+      title: '购买与合作',
+      links: [
+        {
+          text: '购买咨询',
+          href: '/',
+        },
+        {
+          text: '合作洽谈',
+          href: '/',
+        },
+      ],
+    },
+    {
+      title: '服务与支持',
+      links: [
+        {
+          text: '客户服务',
+          href: '/',
+        },
+        {
+          text: '帮助中心',
+          href: '/',
+        },
+        {
+          text: '资源与下载中心',
+          href: '/',
+        },
+        {
+          text: '服务条款与政策',
+          href: '/',
+        },
       ],
     },
   ],
@@ -112,6 +187,6 @@ export const footerData = {
   ], */
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/images/favicon.svg" alt="kippal logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://www.kippal.ai/">KIPPAL Robotics</a> · All rights reserved.
+    2025 <a class="text-blue-500 underline dark:text-muted" href="https://www.kippal.ai/">KIPPAL Robotics</a>  版权所有
   `,
 };
